628dab9ed28708c472f2510f988572ca4a563c84
WorldZProductions <luanbraga2003@gmail.com>
2022-09-28 19:54:25 -0300

    rota de formulario

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask("projeto")
 @app.route("/")
 def ola_mundo():
@@ -13,7 +13,24 @@ def ola_mundo():
     
 # Nova Rota Teste
 @app.route("/teste")
-def funcao_teste():
-    return "Nova rota teste", 200
-    
+@app.route("/teste/<variavel>")
+def funcao_teste(variavel = ""):
+    return "Nova rota teste<br>Variável: {}".format(variavel), 200
+
+
+# Rota formulário
+@app.route("/form")
+def form():
+    return render_template("form.html"), 200
+
+# Rota tratamento do formulário
+@app.route("/form_recebe", methods=["GET", "POST"])
+def form_recebe():
+    nome = ""
+    if request.method == "POST":
+        nome = request.form["nome"]
+        return "Nome: {}".format(nome), 200
+    else:
+        return "Não pode chamar direto no GET", 200
+
 app.run()
