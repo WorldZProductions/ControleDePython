8adc4148f5291cc93ca8bf2d2af8c8317979320f
WorldZProductions <luanbraga2003@gmail.com>
2022-09-28 13:07:40 -0300

    app.py Alteração 2

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask("projeto")
 @app.route("/")
 def ola_mundo():
-    return "Olá Mundo! Bem Vindo ao Teste no novo projeto!!", 200
+    return "Olá Mundo! Bem Vindo ao Teste do novo projeto!!", 200
 
 app.run()
